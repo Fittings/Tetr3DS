6d255ae76e197d0d49ae997614789bf1da65ec72
cmilsom <cameron.live@gmail.com>
2016-09-26 21:34:01 +0100

    Added in the Region struct.
    This is going to be used for determining view locations.
    Part of the changes to decouple view logic from the presenter/models.

---
+++ include/general/region/region.h
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_GAME_REGION_H_
+#define INCLUDE_GAME_REGION_H_
+
+#include <3ds.h>
+
+typedef struct _Region Region;
+
+Region *region_init(Point *top_left_corner, u16 width, u16 height);
+extern void region_free(Region *self);
+
+extern u16 region_get_width(Region *self);
+extern u16 region_get_height(Region *self);
+
+extern void region_set_width(Region *self, u32 width);
+extern void region_set_height(Region *self, u32 height);
+
+extern void region_set_top_left_corner(Region *self, Point *top_left_corner);
+extern void region_set_top_left_x(Region *self, u16 x);
+extern void region_set_top_left_y(Region *self, u16 y);
+
+#endif
---
+++ source/general/region/region.c
@@ -0,0 +1,76 @@
+#include "../include/general/region/region.h"
+
+#include <malloc.h>
+
+struct _Region
+{
+	Point *top_left_corner;
+
+	u16 width;
+	u16 height;
+};
+
+
+Region *region_init(Point *top_left_corner, u16 width, u16 height)
+{
+	Region *self = malloc(sizeof *self);
+	if (!self) return NULL;
+	{
+		self->top_left_corner = top_left_corner;
+
+		self->width = width;
+		self->height = height;
+	}
+
+	return self;
+}
+
+void region_free(Region *self)
+{
+	if (self)
+	{
+		point_free(self->top_left_corner);
+		free(self);
+	}
+}
+
+
+u16 region_get_width(Region *self)
+{
+	return self->width;
+}
+
+u16 region_get_height(Region *self)
+{
+	return self->height;
+}
+
+void region_set_width(Region *self, u32 width)
+{
+	self->width = width;
+}
+
+void region_set_height(Region *self, u32 height)
+{
+	self->height = height;
+}
+
+void region_set_top_left_corner(Region *self, Point *top_left_corner)
+{
+	point_free(self->top_left_corner);
+	self->top_left_corner = top_left_corner;
+}
+
+void region_set_top_left_x(Region *self, u16 x)
+{
+	Point *new_top_left_corner = point_init(x, point_get_y(self->top_left_corner));
+
+	region_set_top_left_corner(self, new_top_left_corner);
+}
+
+void region_set_top_left_y(Region *self, u16 y)
+{
+	Point *new_top_left_corner = point_init(point_get_x(self->top_left_corner), y);
+
+	region_set_top_left_corner(self, new_top_left_corner);
+}
